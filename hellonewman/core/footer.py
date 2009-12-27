d985ffee4c39ebfaee8822b08b6c53ca36b185c0
Greg Newman <greg@20seven.org>
2009-12-27 06:40:43 -0500

    style and contxt_processor changes

--- hellonewman/core/footer.py
+++ hellonewman/core/footer.py
@@ -4,8 +4,6 @@ from syncr.readernaut.models import Book
 
 def footer_feeds(request):
     flickr_photos = Photo.objects.all()[:6]
-    tweets = Tweet.objects.all().order_by("-pub_time")[:5]
-    books = Book.objects.all()[:2]
+    books = Book.objects.all()[:4]
     return {'flickr_photos': flickr_photos,
-            'tweets': tweets,
             'books': books,}
