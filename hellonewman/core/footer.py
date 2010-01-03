8856b27edd3536c5bccab217c5f07a41ef9e24a0
Greg Newman <greg@20seven.org>
2010-01-03 10:19:24 -0500

    and more styling...

--- hellonewman/core/footer.py
+++ hellonewman/core/footer.py
@@ -3,7 +3,5 @@ from syncr.flickr.models import Photo
 from syncr.readernaut.models import Book
 
 def footer_feeds(request):
-    flickr_photos = Photo.objects.all()[:6]
-    books = Book.objects.all()[:4]
-    return {'flickr_photos': flickr_photos,
-            'books': books,}
+    books = Book.objects.all()[:8]
+    return {'books': books,}
